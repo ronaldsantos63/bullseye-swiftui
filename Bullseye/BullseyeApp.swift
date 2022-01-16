91ae9d7a0bc72c57a0a99cd8a0de946631c7fefc
Ronald Santos <ronaldsantos63@gmail.com>
2022-01-16 01:05:09 -0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Ronald Ferreira Dos Santos on 16/01/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
